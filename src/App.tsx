e6212a53ae9a82219d326803dc8d1d96e5beeabc
Kevin Metzdorf <kevin.metzdorf@icloud.com>
2023-12-11 16:45:22 +0100

    update todo und delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,56 @@
-// app.tsx
+// src/App.tsx
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import TodoList from './TodoList';
-import TodoForm from './TodoForm'; // Hinzugefügt
-import './styles.css';
-import { Todo } from './Todo';
+
+interface Todo {
+    id: string;
+    description: string;
+    status: string;
+}
 
 const App: React.FC = () => {
     const [todos, setTodos] = useState<Todo[]>([]);
+    const [newTodo, setNewTodo] = useState<string>('');
 
     useEffect(() => {
-        getAllTodos();
-    }, []);
-
-    const getAllTodos = () => {
-        axios.get<Todo[]>('/api/todo')
+        axios.get('/api/todo')
             .then(response => setTodos(response.data))
             .catch(error => console.error('Fehler beim Laden der Todos:', error));
+    }, []);
+
+    const handleAddTodo = () => {
+        const todoToAdd: Todo = {
+            id: Math.random().toString(36).substring(7),
+            description: newTodo,
+            status: 'OPEN',
+        };
+
+        axios.post('/api/todo', todoToAdd)
+            .then(response => setTodos([...todos, response.data]))
+            .catch(error => console.error('Fehler beim Hinzufügen des Todos:', error));
     };
 
-    const addTodo = (newTodo: Todo) => {
-        axios.post<Todo>('/api/todo', newTodo)
-            .then(response => {
-                setTodos([...todos, response.data]);
-            })
-            .catch(error => console.error('Fehler beim Hinzufügen eines neuen Todos:', error));
+    const handleDeleteTodo = (id: string) => {
+        axios.delete(`/api/todo/${id}`)
+            .then(() => setTodos(todos.filter(todo => todo.id !== id)))
+            .catch(error => console.error('Fehler beim Löschen des Todos:', error));
     };
 
     return (
         <div>
-            <h1>Todo App</h1>
-            <TodoList todos={todos} />
-            <TodoForm onAddTodo={addTodo} />
+            <h1>Todo List</h1>
+            <ul>
+                {todos.map(todo => (
+                    <li key={todo.id}>
+                        {todo.description} - {todo.status}
+                        <button onClick={() => handleDeleteTodo(todo.id)}>Löschen</button>
+                    </li>
+                ))}
+            </ul>
+            <div>
+                <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
+                <button onClick={handleAddTodo}>Hinzufügen</button>
+            </div>
         </div>
     );
 };
